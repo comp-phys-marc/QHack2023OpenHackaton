2d52d059dddec054dedc7b4303ab6df61c9afcfe
Harshit Gupta <harshit.11235@gmail.com>
2023-02-23 22:24:27 +0530

    make flask simulate endpoint

--- simulationservice/service/app.py
+++ simulationservice/service/app.py
@@ -1,6 +1,6 @@
 import json
 import signal
-import sys
+import os
 from flask import Flask, request, abort
 from flask_cors import CORS
 from kombu import Queue
@@ -13,99 +13,145 @@ import strawberryfields as sf
 
 monkey.patch_all()
 
+# Application specific
 app = Flask(__name__)
 CORS(app)
-
 settings = Settings()
 
-@app.route('/auth', methods=['POST', 'PUT'])
-def auth():
-    print(request.data, sys.stderr)
-
-    data = json.loads(request.data.decode("utf-8"))
-
-    if request.method == 'POST':
-
-        response, status = new_auth_response(data)
-        return response, status
-
-    elif request.method == 'PUT':
-
-        old_auth_token = data[TOKEN_KEY]
-
-        try:
-
-            if REFRESH_TOKEN_KEY in data:
-                refresh_token = data[REFRESH_TOKEN_KEY]
-            else:
-                raise Exception
-
-            decoded_auth_token = AuthGuard.decode_token(old_auth_token)
-
-            try:
-                AuthGuard.check_token_expired(old_auth_token)
-
-            except ExpiredSignatureError:
-
-                decoded_refresh_token = AuthGuard.decode_token(refresh_token)
-
-                if not decoded_refresh_token[TOKEN_KEY] == old_auth_token \
-                        or AuthGuard.check_token_expired(refresh_token):
-                    raise Exception
-
-            user_id = str(decoded_auth_token['id'])
-            user = # TODO: get DEFAULT_USER
-
-            if user is not None:
-                return json.dumps(AuthGuard.auth_response(user['data'])), 200
-            else:
-                abort(403)
-
-        except Exception as ex:
-            print(str(ex))
-            abort(401)
+# Supporting backends
+supported_backends = ["fock", "gaussian"]
 
 
-@app.route('/simulate', methods=['POST'])
+@app.route("/simulate", methods=["POST"])
 def simulate():
+    data = request.json
+    response = {"result": None, "error": None, "status": None}
+    if "code" not in data:
+        response["status"] = 400
+        response["error"] = "No code given to execute"
+        return json.dumps(response)
+    # try:
+    #     user_id = AuthGuard.authenticate(data)
+    # except Exception as ex:
+    #     abort(401)
 
-    data = json.loads(request.data.decode("utf-8"))
-    user_id = None
+    # should be a LIST OF LINES
+    # {
+    #  "code" : ["a", "b", "c"]
+    # }
+    sent_code = data["code"]
 
-    if 'code' not in data or 'name' not in data:
-        abort(400)
+    response["status"] = 200
 
+    # save the received string to a temp file
+    with open("temp.xbb", "w") as fp:
+        for line in sent_code:
+            fp.write(line + "\n")
     try:
-        user_id = AuthGuard.authenticate(data)
+        blackbird_repr = sf.load("temp.xbb")
+        # try the code execution on a provided simulator backend
+        if blackbird_repr.target not in supported_backends:
+            if blackbird_repr.target:
+                error_msg = "Given backend is not supported"
+            else:
+                error_msg = "No backend is specified"
+            response["error"] = error_msg
 
-    except Exception as ex:
-        abort(401)
+        else:
+            engine = sf.Engine(
+                backend=blackbird_repr.target,
+                backend_options=blackbird_repr.backend_options,
+            )
+            results = engine.run(blackbird_repr)
+            print("Success!\n Results : ", results)
+            print(results.samples)
 
-    # response = TODO: use StrawberryFields here
+            # this will be list of lists
+            response_measurement = []
+            for sample in results.samples:
+                curr_list = []
+                for value in sample:
+                    if isinstance(value, (int, float)):
+                        str_val = str(value)
+                    else:
+                        str_val = f"{value.real}+j{value.imag}"
+                    curr_list.append(str_val)
+                response_measurement.append(curr_list)
 
-    if response is not None:
-        return json.dumps(response), response['status']
-    else:
-        abort(403)
+            response["result"] = {"measurements": response_measurement}
+    except Exception as err:
+        response[
+            "error"
+        ] = f" The following exception occurred during the execution of your program - \n{err}"
+
+    os.remove("temp.xbb")
+    return json.dumps(response)
 
 
-def new_auth_response(user_data):
-    username = user_data['name']
-    password = user_data['password']
-    # response = TODO: login(username, password)
+# @app.route('/auth', methods=['POST', 'PUT'])
+# def auth():
+#     print(request.data, sys.stderr)
 
-    if 'data' in response.keys():
-        user = response['data']
-        return json.dumps(AuthGuard.auth_response(user)), response['status']
-    else:
-        return json.dumps(response), response['status']
+#     data = json.loads(request.data.decode("utf-8"))
+
+#     if request.method == 'POST':
+#         response, status = new_auth_response(data)
+#         return response, status
+
+#     elif request.method == 'PUT':
+
+#         old_auth_token = data[TOKEN_KEY]
+
+#         try:
+
+#             if REFRESH_TOKEN_KEY in data:
+#                 refresh_token = data[REFRESH_TOKEN_KEY]
+#             else:
+#                 raise Exception
+
+#             decoded_auth_token = AuthGuard.decode_token(old_auth_token)
+
+#             try:
+#                 AuthGuard.check_token_expired(old_auth_token)
+
+#             except ExpiredSignatureError:
+
+#                 decoded_refresh_token = AuthGuard.decode_token(refresh_token)
+
+#                 if not decoded_refresh_token[TOKEN_KEY] == old_auth_token \
+#                         or AuthGuard.check_token_expired(refresh_token):
+#                     raise Exception
+
+#             user_id = str(decoded_auth_token['id'])
+#             user = # TODO: get DEFAULT_USER
+
+#             if user is not None:
+#                 return json.dumps(AuthGuard.auth_response(user['data'])), 200
+#             else:
+#                 abort(403)
+
+#         except Exception as ex:
+#             print(str(ex))
+#             abort(401)
+
+
+# def new_auth_response(user_data):
+#     username = user_data['name']
+#     password = user_data['password']
+#     # response = TODO: login(username, password)
+
+#     if 'data' in response.keys():
+#         user = response['data']
+#         return json.dumps(AuthGuard.auth_response(user)), response['status']
+#     else:
+#         return json.dumps(response), response['status']
 
 
 def _signal_handler(param1, param2):
     exit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     signal.signal(signal.SIGINT, _signal_handler)
-    http_server = WSGIServer(('0.0.0.0', 5000), app.wsgi_app)
+    http_server = WSGIServer(("0.0.0.0", 5000), app.wsgi_app)
     http_server.serve_forever()
