d2d79a65de8e4bcde3cf1137cddacd68b46d6f8a
Marcus Edwards <edwardsmarc968@gmail.com>
2023-02-26 12:59:21 -0800

    Frontend logic

--- editor/run.js
+++ editor/run.js
@@ -2,27 +2,40 @@ const blackbird = require('blackbird-ts');
 const editor = $('#code-editor');
 const codeDebugger = $('#code-debugger');
 
-function remoteExecute(text) {
-    $('#load-text').text(text);
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
+function remoteExecute(block) {
     $('#loading-div').show();
-    $.post('https://120.0.0.1:5432/' + $('#request-text').val(),  // TODO: replace with query to simulationservice
+    $.post('https://120.0.0.1:5000/simulate',
         {
-            _api_key: '$2y$10$5a1NNdQKUuQyvMwxf.KC0.RLf60fwaPR/v0/raLyGrnoHCjdckfE2'
+            code: block
         })
         .done(function (data) {
-            alert(data.message);
-            $('#sfdev-container').append("<div class='widget-wrapper'>" + data.widget + "</div>");
-            $('#loading-div').hide();
+            $('#sfdev-container').append("<div class='widget-wrapper'>" + syntaxHighlight(JSON.stringify(data, undefined, 4)) + "</div>");
         })
         .fail(function(data){
-            if(data.hasOwnProperty('message')){
-                alert(data.message);
+            if(data.hasOwnProperty('error')){
+                alert(data.error);
             }
-            $('#loading-div').hide();
-        })
-        .always(function(){
-            $('#loading-div').hide();
-        });
+        }
+    );
 }
 
 $(document).ready(function(){
@@ -32,21 +45,22 @@ $(document).ready(function(){
         let javascriptBlocks = {};
         try {
             const parse = blackbird.parseString;
-            let blackbirdBlock = "\n";
-            let javascriptBlock = "\n";
+            let blackbirdBlock = [];
+            let javascriptBlock = [];
             for (let line of editor.text().split("\n")) {
-                if (line == "" && blackbirdBlock != "\n") {
+                if (line == "" && blackbirdBlock != []) {
                     blackbirdBlocks[Object.keys(blackbirdBlocks).length] = blackbirdBlock;
-                    blackbirdBlock = "\n";
-                } else if (line == "" && javascriptBlock != "\n") {
-                    blackbirdBlocks[Object.keys(javascriptBlocks).length] = javascriptBlock;
-                    blackbirdBlock = "\n";
+                    blackbirdBlock = [];
+                } else if (line == "" && javascriptBlock != []) {
+                    javascriptBlocks[Object.keys(javascriptBlocks).length] = javascriptBlock;
+                    javascriptBlock = [];
                 }
-                let blackbirdLine = parse(line);
-                if (blackbirdLine != []) {  // TODO: verify 
-                    blackbirdBlock += blackbirdLine + "\n";
-                } else {
-                    javascriptBlock += line + "\n";
+                try {
+                    let blackbirdLine = parse(line);
+                    blackbirdBlock += [blackbirdLine];
+                }
+                catch {
+                    javascriptBlock += [line];
                 }
             }
         } catch (e) {
@@ -56,8 +70,13 @@ $(document).ready(function(){
             }
         }
 
-        // TODO: execute the JS and the BlackBird asynchronously
-        
+        for (let blockId of Object.keys(blackbirdBlocks)) {
+            remoteExecute(blackbirdBlocks[blockId]);
+        }
 
+        for (let blockId of Object.keys(javascriptBlocks)) {
+            let res = eval(javascriptBlocks[blockId]);
+            codeDebugger.text(res);
+        }
     });
 });
